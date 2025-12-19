0f6c38599abf8c60e921b96f9eac0c0235b46f94
SavannahSoliel <srhamley05@gmail.com>
2025-12-19 18:39:18 -0500

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -3,21 +3,21 @@ export const products = [
       id: 1,
       name: "Soft Pink Lip Gloss",
       brand: "Glossier",
-      link: "https://www.glossier.com",
+      link: "https://www.glossier.com/products/lip-gloss?variant=45798167675125",
       tags: ["soft", "feminine", "pastel"]
     },
     {
       id: 2,
       name: "Minimal Gold Necklace",
       brand: "Etsy",
-      link: "https://www.etsy.com",
+      link: "https://www.etsy.com/listing/1797455914/gold-plated-enamel-sakura-flower?ls=s&ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=gold+necklace&ref=sr_gallery-1-27&pro=1&frs=1&pop=1&content_source=6046fb20-7973-4d26-b4c9-a38d32d5ad54%253ALT1ffac3a8d5b0677b3f7229bce63f7c2307347b26&organic_search_click=1&logging_key=6046fb20-7973-4d26-b4c9-a38d32d5ad54%3ALT1ffac3a8d5b0677b3f7229bce63f7c2307347b26",
       tags: ["clean", "romantic", "minimal"]
     },
     {
       id: 3,
       name: "Vintage Knit Cardigan",
       brand: "Urban Outfitters",
-      link: "https://www.urbanoutfitters.com",
+      link: "https://www.urbanoutfitters.com/shop/bdg-becca-brushed-knit-oversized-cardigan?color=006&searchparams=q%3Dcardigan",
       tags: ["vintage", "cozy", "muted"]
     }
   ];
