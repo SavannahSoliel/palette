306fa76e430ef3e6dde620cf0a7ad55e8cd68b75
SavannahSoliel <srhamley05@gmail.com>
2025-12-25 01:21:56 -0500

    sign in ui

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,21 +2,11 @@ import './UserProfile.css';
 
 function UserProfile({ user, onSignOut }) {
   return (
-    <div className="user-pill">
-  <div className="user-avatar">
-    {user.email[0].toUpperCase()}
-  </div>
-
-  <div className="user-meta">
-    <span className="user-name">
-      {user.user_metadata?.full_name || 'you'}
-    </span>
-    <button className="sign-out" onClick={onSignOut}>
-      sign out
+    <button className="user-avatar-btn" onClick={onSignOut} aria-label="Sign out">
+      <div className="user-avatar">
+        {user.email[0].toUpperCase()}
+      </div>
     </button>
-  </div>
-</div>
-
   );
 }
 
